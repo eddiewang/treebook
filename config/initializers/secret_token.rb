db2f88cc6859ca2fdae21694e958ca94df530bc2
Eddie Wang <eddie@eddiewang.me>
2013-02-23 23:09:40 -0500

    First commit on Windows

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '7fa3ae2c97e5a6e9e8492d9584c0e9191a141df2b959bf5412b1a70ab85c8430de1cf3cdd85982eae3bffd928515c9b1c7231787d32e6915a7f7512808366e63'
